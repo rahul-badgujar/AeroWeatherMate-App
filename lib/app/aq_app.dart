6a92409c6b81a0003a18ad51b5dea680cb4cca96
Rahul Badgujar <44890430+imRB7here@users.noreply.github.com>
2020-09-01 19:45:01 +0530

    boilerplate code added

---
+++ lib/app/aq_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:air_quality_app/app/pages/main_page.dart';
+
+class AqApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Air Quality App",
+      home: MainPage(),
+    );
+  }
+}
